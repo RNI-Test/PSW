0e41b20e8e54d4376638d3400c2dd1fa5dfd673c
Marija Klještan <marijakljestan@uns.ac.rs>
2021-11-16 22:46:08 +0100

    Update Property

--- TestLibrary/Class1.cs
+++ TestLibrary/Class1.cs
@@ -6,7 +6,7 @@ namespace TestLibrary
     {
         public int MyProperty { get; set; }
         public int MyProperty2 { get; set; }
-        public int MyProperty4 { get; set; }
+        public int MyProperty3 { get; set; }
 
         public Class1()
         {
